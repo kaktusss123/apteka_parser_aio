c277a516c7c7677975882a8b8e42c8351ceaa0c4
Владислав <komarnitskij.v@phystech.edu>
2019-11-03 13:42:20 +0300

    try-except

--- main.py
+++ main.py
@@ -14,14 +14,23 @@ def test():
 
 @app.route('/vivafarm', methods=['GET'])
 def vivafarm():
-    loop.run_until_complete(parse_vivafarm())
+    global loop
+    try:
+        loop.run_until_complete(parse_vivafarm())
+    except Exception as e:
+        return '{}: {}'.format(e.__class__.__name__, e)
     return send_from_directory('./', 'vivafarm.py.csv')
 
 
 @app.route('/extractum', methods=['GET'])
 def extractum():
-    loop.run_until_complete(parse_extractum())
+    global loop
+    try:
+        loop.run_until_complete(parse_extractum())
+    except Exception as e:
+        return '{}: {}'.format(e.__class__.__name__, e)
     return send_from_directory('./', 'extractum.py.csv')
 
 
-app.run('45.147.198.134', 9512)
+app.run()
+# app.run('45.147.198.134', 9512)
