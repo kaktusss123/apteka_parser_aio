66c1588c6aa774360e93810aaafca9c12c69bc94
Владислав <komarnitskij.v@phystech.edu>
2019-11-03 14:09:52 +0300

    error hendle

--- main.py
+++ main.py
@@ -14,20 +14,20 @@ def test():
 @app.route('/vivafarm', methods=['GET'])
 def vivafarm():
     loop = asyncio.new_event_loop()
-    try:
-        loop.run_until_complete(parse_vivafarm(loop))
-    except Exception as e:
-        return '{}: {}'.format(e.__class__.__name__, e)
+    # try:
+    loop.run_until_complete(parse_vivafarm(loop))
+    # except Exception as e:
+    #     return '{}: {}'.format(e.__class__.__name__, e)
     return send_from_directory('./', 'vivafarm.py.csv')
 
 
 @app.route('/extractum', methods=['GET'])
 def extractum():
     loop = asyncio.new_event_loop()
-    try:
-        loop.run_until_complete(parse_extractum(loop))
-    except Exception as e:
-        return '{}: {}'.format(e.__class__.__name__, e)
+    # try:
+    loop.run_until_complete(parse_extractum(loop))
+    # except Exception as e:
+    #     return '{}: {}'.format(e.__class__.__name__, e)
     return send_from_directory('./', 'extractum.py.csv')
 
 
